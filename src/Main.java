05614e14b76a4f0e0e977752e8e32ff530c34dc6
Li2nga <irina.zaitceva.iz@gmail.com>
2023-07-16 14:27:05 +0300

    Done 1.01 добавлена задача5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -71,6 +72,34 @@ public class Main {
             System.out.println("Доставки нет");
         } else {
             System.out.println("Потребуется дней: " + deliveryTime);
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого сезона не существует");
         }
     }
 }
